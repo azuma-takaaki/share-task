22ae13a0b19bf4a2f8bc860bb445ce9c79a0f8a2
azuma <azuma.takaaki.1000@gmail.com>
2021-03-02 23:35:21 +0900

    add login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def login_page
   end
+
+  def login
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to  user_path(user)
+    else
+      redirect_to top_path
+    end
+  end
 end
