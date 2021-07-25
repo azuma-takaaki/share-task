3c0f5002c0ee3ae89a9733993241ad06af0e1e7d
azuma <azuma.takaaki.1000@gmail.com>
2021-07-25 11:21:15 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+    User.create(
+      email: "test_user#{n + 1}@test.com",
+      name: "テストユーザー#{n + 1}",
+      password: "test_user#{n + 1}",
+      icon:"icon_"+rand(0...10).to_s+".png"
+    )
+end
