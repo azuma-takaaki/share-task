c39907ae48103fde1ce2e241d2396aa34b8a81d0
azuma <azuma.takaaki.1000@gmail.com>
2021-07-31 21:12:19 +0900

    first integrate test

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,51 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "User", type: :model do
-  describe "#create" do
-    before do
-      @user = FactoryBot.build(:user)
-    end
-
-    example "正しい情報は登録ができる" do
-      expect(@user.valid?).to eq true
-    end
-
-    example "名前が空だと登録できない" do
-      @user.name = nil
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Name can't be blank")
-    end
-
-    example "emailが空だと登録できない" do
-      @user.email = nil
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
-    end
-
-    example "passwordが空だと登録できない" do
-      @user.password = nil
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
-    end
-
-    example "iconが空だと登録できない" do
-      @user.icon = nil
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Icon can't be blank")
-    end
-
-
-    example "emailが256文字以上だと登録できない" do
-      @user.email = "a" * 250+ "@a.com"
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email is too long (maximum is 255 characters)")
-     end
-
-     example "passwordが6文字未満だと登録できない" do
-       @user.password = "a" * 5
-       @user.password_confirmation = "a" * 5
-       @user.valid?
-       expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
-     end
+feature "Signup" , :js => true do
+  example "新規登録後にユーザーページが表示される" do
+    visit "/"
+    click_button("新規アカウント登録")
+    fill_in 'user_name', with:'test_user'
+    fill_in 'email', with:'hogehoge@hoge.com'
+    fill_in 'password', with:'password'
+    fill_in 'password_confirm', with:'password'
+    click_button("登録")
+    visit "/"
   end
 end
