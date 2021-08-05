4d6150394aeacb84ec57800928b11820a2b08bf3
azuma <azuma.takaaki.1000@gmail.com>
2021-08-05 20:46:35 +0900

    add the ability to create and delete castles

--- app/javascript/components/Catsle.jsx
+++ app/javascript/components/Catsle.jsx
@@ -6,13 +6,6 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import PropTypes from "prop-types";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-const LoadModel = () => {
-	const gltf = useLoader(GLTFLoader, " /catsle.glb")
-	return (
-		<primitive scale={[0.07, 0.07, 0.07]}  object={gltf.scene} dispose={null} />
-	)
-}
-
 const CameraController = () => {
   const { camera, gl } = useThree();
   useEffect(
@@ -30,13 +23,23 @@ const CameraController = () => {
   return null;
 };
 
-const UseModel = () => {
+const LoadModel = (modelpath) => {
+	const gltf = useLoader(GLTFLoader, modelpath.modelpath)
 	return (
-		<Suspense fallback={null}>
-				<LoadModel />
-		</Suspense>
+		<primitive scale={[0.07, 0.07, 0.07]}  object={gltf.scene.clone(true)} dispose={null} />
 	)
 }
+
+var UseModel = (position) =>{
+	return (
+		<mesh  {...position} rotation={[0, -Math.PI / 2, 0]}>
+			<Suspense fallback={null}>
+					<LoadModel modelpath=" /catsle.glb"/>
+			</Suspense>
+		</mesh>
+	)
+}
+
 
 
 function Catsle(){
@@ -47,24 +50,54 @@ function Catsle(){
 	const [countPosY,setCountPosY] = useState(0)
 	const [countPosZ,setCountPosZ] = useState(0)
   const [rotY, setPosCount] = useState(0)
+	const [newCatslePos, setCatslePos] = useState(0)
   const handleClick = () => {
     setPosCount(rotY+1)
   };
 
+	const addCatsle = () => {
+		setCountPosX(countPosX+1)
+ }
+
+ const destroyCatsle = () => {
+	 setCountPosX(countPosX-1)
+}
+
   return (
     <div id="catsle">
 
-  		<Canvas  >
-			<CameraController />
-  			<Camera position={[countPosX, 4+countPosY, 10+countPosZ]}   rotation={[Math.PI/24*(countRotX-3), Math.PI/24*countRotY, Math.PI/24*countRotZ]}/>
+			<div id="add-catsle-button" >
+				<p>catsle: {countPosX}</p>
+				<button  onClick={addCatsle}>catsle +</button>
+				<button  onClick={destroyCatsle}>catsle -</button>
+			</div>
+  		<Canvas >
+				<CameraController />
+  			<Camera position={[0, 4, 10]}   rotation={[Math.PI/24*(countRotX-3), Math.PI/24*countRotY, Math.PI/24*countRotZ]}/>
   			<gridHelper args={[100, 100, 0X696969, 0X696969]} position={[0, 0, 0]}/>
   			<pointLight position={[10, -20, 70]} />
 				<pointLight position={[0, 100, -150]} />
-  			<mesh
-  				visible userData={{ hello: 'world' }} position={[0, 0, 0]} rotation={[0, -Math.PI / 2, 0]}
-  			>
-  				<UseModel />
-  			</mesh>
+
+
+
+				{(() => {
+						const catsles = [];
+						var n = 0
+						var l = 0
+						for(var i = 0; i < countPosX; i++) {
+								catsles.push(<UseModel position={[l*3, 0, -i+l*10]} />)
+								if(n===9){
+									l++
+									n=0
+								}else{
+									n++
+								}
+
+						}
+						return(catsles)
+				})()}
+
+
 
 
   		</Canvas>
