2739b21a0f64b4811deaca0b233b832684dfb79f
azuma-takaaki <azuma.takaaki.1000@gmail.com>
2021-10-18 20:00:31 +0900

    add feature test and modify report_controller.rb to edit report content #101

--- spec/features/report_spec.rb
+++ spec/features/report_spec.rb
@@ -15,7 +15,7 @@ feature "Report" , :js => true do
   describe "#valid_infomation" do
     example "新規の積み上げ(report)を追加できる" do
       @group = FactoryBot.create(:programming)
-       page.find('.side-menu-toggle').click
+      page.find('.side-menu-toggle').click
       click_on "グループを探す"
       find("input[placeholder='グループを探す']").set("progra")
       click_button "programming"
@@ -34,7 +34,7 @@ feature "Report" , :js => true do
 
     example "ユーザーページの積み上げにいいねボタンと投稿日時が表示される" do
       @group = FactoryBot.create(:programming)
-       page.find('.side-menu-toggle').click
+      page.find('.side-menu-toggle').click
       click_on "グループを探す"
       find("input[placeholder='グループを探す']").set("progra")
       click_button "programming"
@@ -52,5 +52,29 @@ feature "Report" , :js => true do
       expect(page).to have_selector ".report-wrapper > .report-infomation-wrapper > .like-button-and-created-at-wrapper > .like-button"
       expect(page).to have_selector ".report-wrapper > .report-infomation-wrapper > .like-button-and-created-at-wrapper > .created-at-of-report"
     end
+
+    example "投稿した積み上げの内容を編集できる" do
+      @group = FactoryBot.create(:programming)
+      page.find('.side-menu-toggle').click
+      click_on "グループを探す"
+      find("input[placeholder='グループを探す']").set("progra")
+      click_button "programming"
+      find('div.bm-overlay').click
+      click_button "城を建てる"
+      fill_in "城の名前(目標)", with:"web開発エンジニアになる"
+      find(".post-castle-data-button").click
+      expect(page).to have_content "web開発エンジニアになる 城"
+      page.find('.user-name', text: 'test_user1').click
+      expect(page).to have_selector ".users-page-header-name", text: "test_user1"
+      click_button "積み上げを登録する"
+      fill_in "今日の積み上げ", with:"プログラミングを5時間勉強した！"
+      click_button "登録する"
+      expect(page).to have_selector ".report-content", text: "プログラミングを5時間勉強した！"
+      page.find('.edit-report-button').click
+      fill_in "積み上げを修正", with:"プログラミングを10時間勉強した！"
+      find(".post-new-report-content-button").click
+      expect(page).to have_selector ".report-content", text: "プログラミングを10時間勉強した！"
+    end
+
   end
 end
